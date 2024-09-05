039f15c5a03626733897eaea5ed3b7a2b6d4acfc
Ratish149 <ratish.shakya149@gmail.com>
2024-09-05 18:51:13 +0545

    commit

---
+++ app/views.py
@@ -0,0 +1,33 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import EmailSerializer
+# Create your views here.
+
+class SendEmailView(generics.CreateAPIView):
+    serializer_class = EmailSerializer
+
+    def perform_create(self, serializer):
+        
+        email=serializer.validated_data['email']
+        message=render_to_string('email.html')
+        try:
+            send_mail(
+                subject='Chalao',
+                message='',
+                html_message=message,
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[email],
+                fail_silently=False
+            )
+            return Response({"message": "Email sent successfully"}, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({"error": f"Failed to send email: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+
+
+
