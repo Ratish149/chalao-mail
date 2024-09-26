d972ea8d0b74a2922bdc14776429a473de7648f9
Ratish149 <ratish.shakya149@gmail.com>
2024-09-26 13:07:25 +0545

    update

--- app/views.py
+++ app/views.py
@@ -7,13 +7,16 @@ from .serializers import EmailSerializer
 from .models import Mail
 # Create your views here.
 
-class SendEmailView(generics.CreateAPIView):
+class SendEmailView(generics.ListCreateAPIView):
+    queryset = Mail.objects.all()
     serializer_class = EmailSerializer
 
-    def perform_create(self, serializer):
-        full_name=serializer.validated_data['full_name']
-        email=serializer.validated_data['email']
-        message=render_to_string('email.html')
+    def create(self, request, *args, **kwargs):
+        data=request.data  
+        full_name = data.get['full_name']
+        email = data.get['email']
+        message = render_to_string('email.html')
+        
         try:
             send_mail(
                 subject='Chalao',
@@ -23,14 +26,17 @@ class SendEmailView(generics.CreateAPIView):
                 recipient_list=[email],
                 fail_silently=False
             )
-            mail=Mail.objects.create(full_name=full_name,email=email)
+            mail = Mail.objects.create(full_name=full_name, email=email)
             mail.save()
 
             return Response({"message": "Email sent successfully"}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({"error": f"Failed to send email: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
+
+
+
+
 
 
 
